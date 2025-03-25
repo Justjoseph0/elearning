8123586edba0c9dab1ebc7e7c8719d3a97b7488a
Merlvyn <Akuchiemelvin0@gmail.com>
2025-03-25 15:48:45 +0100

    Initial commit

---
+++ frontend/src/components/AppLayout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom"
+import NavBar from "./NavBar"
+import Footer from "./Footer"
+
+
+const AppLayout = () => {
+  return (
+   <>
+    <NavBar />
+    <Outlet />
+    <Footer />
+   </>
+  )
+}
+
+export default AppLayout
